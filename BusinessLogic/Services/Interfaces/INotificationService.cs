ca8bce6fd7907c8675b7df35cb2cec4ae6c877e8
Dat Le Tien <datletien1352004@gmail.com>
2026-03-02 00:11:35 +0700

    feat(Schedule)Add scheduling and notification system for calendars

---
+++ BusinessLogic/Services/Interfaces/INotificationService.cs
@@ -0,0 +1,24 @@
+using BusinessLogic.DTOs.Request;
+using BusinessLogic.DTOs.Response;
+
+namespace BusinessLogic.Services.Interfaces;
+
+public interface INotificationService
+{
+    Task<ServiceResult<bool>> CreateScheduleNotificationAsync(
+        int actorUserId,
+        CreateScheduleNotificationRequest request,
+        CancellationToken ct = default);
+
+    Task<ServiceResult<PagedResultDto<NotificationItemDto>>> GetMyNotificationsAsync(
+        int userId,
+        int page = 1,
+        int pageSize = 20,
+        CancellationToken ct = default);
+
+    Task<ServiceResult<int>> GetMyUnreadCountAsync(int userId, CancellationToken ct = default);
+
+    Task<ServiceResult<bool>> MarkAsReadAsync(int userId, long notificationId, CancellationToken ct = default);
+
+    Task<ServiceResult<bool>> MarkAllAsReadAsync(int userId, CancellationToken ct = default);
+}
